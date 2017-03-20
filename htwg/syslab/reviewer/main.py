da8b3902099ee8d39b40762320d278c66a951eb2
Simon Wörner <git@simon-woerner.de>
2017-03-20 01:48:05 +0100

    use github api

--- htwg/syslab/reviewer/main.py
+++ htwg/syslab/reviewer/main.py
@@ -1,4 +1,22 @@
+#!/usr/bin/env python3
+
 from cmd import Cmd
+from os.path import expanduser, join
+from typing import Dict, Any
+
+from github import Github
+from ruamel import yaml
+
+
+def read_config(file_path: str) -> Dict[str, Any]:
+    # validate
+    # c = Core(source_file=file_path, schema_files=[CONFIG_SCHEMA_FILEPATH])
+    # c.validate(raise_exception=True)
+
+    with open(file_path) as f:
+        config = yaml.safe_load(f)
+
+    return config
 
 
 class MyPrompt(Cmd):
@@ -9,15 +27,33 @@ class MyPrompt(Cmd):
             name = 'stranger'
         else:
             name = args
-        print("Hello, %s" % name)
+        print('Hello, %s' % name)
 
     def do_quit(self, args):
         """Quits the program."""
-        print("Quitting.")
+        print('Quitting.')
         raise SystemExit
 
 
 if __name__ == '__main__':
+    config = read_config(join(expanduser('~'), '.syslab-reviewer.yml'))
+    g = Github(config['github']['token'])
+
+    user = g.get_user()
+    print(user)
+
+    org = g.get_organization('htwg-syslab-bsys')
+    print(org)
+
+    for repo in org.get_repos():
+        print(repo)
+
+        for pr in repo.get_pulls():
+            print('\t', pr)
+
+            for comment in pr.get_comments():
+                print('\t\t', comment)
+
     prompt = MyPrompt()
     prompt.prompt = '> '
     prompt.cmdloop('Starting prompt...')
