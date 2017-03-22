febe1414ab6e12c2971d8b17ec4671066fe1ae81
Simon Wörner <git@simon-woerner.de>
2017-03-22 02:54:11 +0100

    list all groups with pr, sorted by pr creation

--- README.md
+++ README.md
@@ -3,10 +3,26 @@
 [![build status](https://git.brn.li/htwg-syslab/syslab-reviewer/badges/master/build.svg)](https://git.brn.li/htwg-syslab/syslab-reviewer/commits/master)
 [![coverage report](https://git.brn.li/htwg-syslab/syslab-reviewer/badges/master/coverage.svg)](https://git.brn.li/htwg-syslab/syslab-reviewer/commits/master)
 
+## Config
+
+Create a personal GitHub token at: https://github.com/settings/tokens
+
+Required scopes:  [X] repo (Full control of private repositories) 
+
+```
+cp config/syslab-reviewer.sample.yml ~/.syslab-reviewer.yml
+vim ~/.syslab-reviewer.yml # add github token
+```
+
 ## Usage
 
-*TODO*
+```
+python3 htwg/syslab/reviewer/main.py
+```
 
+This currently list all BSYS and SYSO groups with open pull requests.
+
+Groups sorted by (oldest) pull request creation (asc).
 
 
 ## Development
--- htwg/syslab/reviewer/main.py
+++ htwg/syslab/reviewer/main.py
@@ -2,10 +2,13 @@
 
 from cmd import Cmd
 from os.path import expanduser, join
+from textwrap import indent
 from typing import Dict, Any
 
+from blessings import Terminal
 from github import Github
 from ruamel import yaml
+import re
 
 
 def read_config(file_path: str) -> Dict[str, Any]:
@@ -17,6 +20,10 @@ def read_config(file_path: str) -> Dict[str, Any]:
         config = yaml.safe_load(f)
 
     return config
+
+
+def iprint(text, depth=1, prefix=' '*4):
+    print(indent(text, prefix*depth))
 
 
 class MyPrompt(Cmd):
@@ -35,25 +42,95 @@ class MyPrompt(Cmd):
         raise SystemExit
 
 
-if __name__ == '__main__':
+def github(t):
     config = read_config(join(expanduser('~'), '.syslab-reviewer.yml'))
     g = Github(config['github']['token'])
 
     user = g.get_user()
     print(user)
+    print('You are logged in as: {} ({})'.format(user.name, t.bold(user.login)))
 
-    org = g.get_organization('htwg-syslab-bsys')
+    courses = config['courses']
+    print('courses:\n', ' '.join(courses.keys()))
+
+    for course_name, course in courses.items():
+        print('Course: {} ({})'.format(course_name, course['name']))
+        github_course(course, g, t)
+
+
+def github_course(course, g, t):
+    repo_pattern = re.compile(course['repo_regex'])
+    # repo_hw_pattern = re.compile(course['repo_homework'])
+
+    org = g.get_organization(course['organization'])
     print(org)
 
+    repos = {}
+
     for repo in org.get_repos():
-        print(repo)
+        if not repo_pattern.fullmatch(repo.name):
+            continue
 
-        for pr in repo.get_pulls():
-            print('\t', pr)
+        # add repo
+        print(repo.name)
+        repos[repo.name] = repo
+        repo.pull_requests = {}
+        # repos[repo.name]['pull_requests'] = {}
 
-            for comment in pr.get_comments():
-                print('\t\t', comment)
+        for pr in repo.get_pulls(sort='created', direction='asc'):  # state='closed'
+            # add pr
+            repo.pull_requests[pr.number] = pr
+            # repos[repo.name]['pull_requests'][pr.number] = pr
 
-    prompt = MyPrompt()
-    prompt.prompt = '> '
-    prompt.cmdloop('Starting prompt...')
+    # sort by creation of first (open) pull request
+    repos_with_pr = [repo for repo in repos.values() if len(repo.pull_requests) > 0]
+    for repo in sorted(repos_with_pr, key=lambda k: list(k.pull_requests.values())[0].created_at):
+        # print(repo)
+        depth = 1
+
+        print('\n{}: (forks: {}, last push: {})'.format(repo.name, repo.forks_count, repo.pushed_at))
+        iprint('ssh: {}'.format(repo.ssh_url), depth)
+
+        for pr in repo.pull_requests.values():
+            # print('\t', pr)
+            # pr_issue = repo.get_issue(pr.number)
+
+            depth = 1
+            iprint('pr {}: {} (created: {}, updated: {}, assignees: {})'.format(t.bold(str(pr.number)), pr.title, pr.created_at,pr.updated_at, ', '.join(pr.assignees)), depth)
+            depth = 2
+            iprint('{}'.format(pr.html_url), depth)
+            iprint('state: {}, merge status: {}'.format(t.bold(pr.state), pr.mergeable_state), depth)
+            iprint('+ {}, - {} ({} files changed)'.format(pr.additions, pr.deletions, pr.changed_files), depth)
+
+            # set assignee
+            # pr_issue.edit(assignee=user)
+
+            # # details
+            # iprint('{}'.format(pr.body), depth)
+            #
+            # # pr_issue.get_comments()
+            #
+            # for comment in pr.get_comments():
+            #     # iprint(comment, depth)
+            #     depth += 1
+            #     iprint('{}'.format(comment.path))
+            #     iprint('{}'.format(comment.diff_hunk))
+            #     depth += 1
+            #     iprint('{}: {}'.format(t.bold(comment.user.login), comment.body), depth)
+            #     iprint('')
+
+
+def main():
+    term = Terminal()
+
+    # with term.fullscreen():
+    #     github(term)
+
+    github(term)
+
+    # prompt = MyPrompt()
+    # prompt.prompt = '> '
+    # prompt.cmdloop('Starting prompt...')
+
+if __name__ == '__main__':
+    main()
